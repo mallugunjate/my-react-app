ee29e9e5e41ca78d76e8c97a30ac862ab4a59628
unknown <mallu048@gmail.com>
2020-04-08 12:27:11 +0530

    fixx5

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,11 +3,13 @@ import React, { Component, Children } from 'react';
 class Todos extends Component {
  render() {
    var todoData = this.props;
+   console.log(todoData);
    return (
      <div>
        <ul>
          {todoData.map(item=>
           <li>{title}</li>
+
           )}
        </ul>
      </div>
